42b8d9a4f7fa9201399930db25a7d4b1191ed4af
jordan larcher <jordan.larcher.lds@gmail.com>
2026-05-16 09:59:28 -0300

    project update: updated contact validation

---
+++ CSE341-Contacts-part2/validator/contactValidator.js
@@ -0,0 +1,21 @@
+const { body } = require("express-validator");
+
+const createContact = [
+    body('firstname').trim().notEmpty().withMessage('First name is required'),
+    body('lastname').trim().notEmpty().withMessage('Last name is required'),
+    body('email').trim().isEmail().withMessage('Valid email is required'),
+    body('phone').optional().trim().isString(),
+    body('favoriteColor').optional().trim().isString(),
+    body('birthday').optional().trim().isISO8601().withMessage('Birthday must be a valid date'),
+];
+
+const updateContact = [
+    body('firstName').optional().trim().notEmpty(),
+    body('lastName').optional().trim().notEmpty(),
+    body('email').optional().trim().isEmail(),                                                                                                                                             body('phone').optional().trim().isString(),
+    body('favoriteColor').optional().trim().isString(),
+    body('birthday').optional().trim().isISO8601().withMessage('Birthday must be a valid date'),
+];
+
+
+module.exports = { createContact, updateContact };
