853d0e29f6baf2d4cddf41c966ea64a61eb85a4a
jordan larcher <jordan.larcher.lds@gmail.com>
2026-05-16 08:02:13 -0300

    Adding template project files

--- CSE341-API-Assignment/server.js
+++ CSE341-API-Assignment/server.js
@@ -1,4 +1,4 @@
-require('dns').setServers(['8.8.8.8', '1.1.1.1']);
+//require('dns').setServers(['8.8.8.8', '1.1.1.1']);
 
 const app = require('./app');
 const { initDb } = require('./db/connect');
