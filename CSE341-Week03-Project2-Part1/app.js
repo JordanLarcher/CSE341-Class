1a245b1e77730af3faf042c16ebd6517ebc6d75a
jordan larcher <jordan.larcher.lds@gmail.com>
2026-05-23 18:52:12 -0300

    swagger file updated

--- CSE341-Week03-Project2-Part1/app.js
+++ CSE341-Week03-Project2-Part1/app.js
@@ -15,8 +15,9 @@ app.use(express.urlencoded({ extended: true }));
 // CORS 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
     next();
 });
 
